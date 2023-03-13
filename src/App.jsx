3c8552ec6e8270a9e36b325819af715ee4fbd815
Muhammad Yusuf <89022826+m-yusuf-malik@users.noreply.github.com>
2023-03-13 14:09:28 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+
+function App() {
+  const dispatch = useDispatch();
+  const url = useSelector((state) => state.home.url);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
+  };
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  return <div>{url.total_pages}</div>;
+}
+
+export default App;
